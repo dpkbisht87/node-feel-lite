ede6131baa0dcb1546f212e7f0fd17fb85b75a9b
deepak.bisht <deepak.bisht@oracle.com>
2021-03-21 22:37:59 +0530

    Validate connect string

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const express = require('express');
 require('express-async-errors');
 const app = express();
 
+require('./startup/config')()
 require('./startup/logging')()
 require('./startup/routes')(app);
 require('./startup/db')()
-require('./startup/config')()
 require('./startup/validation')();
 require('./startup/prod')(app);
 
--- startup/config.js
+++ startup/config.js
@@ -4,4 +4,7 @@ module.exports = function () {
     if (!config.get('jwtPrivateKey')) {
         throw new Error('FATAL: jwtPrivateKey is not defined');
     }
+    if (!config.get('db')) {
+        throw new Error('FATAL: mongo db connect string is not defined');
+    }
 };
